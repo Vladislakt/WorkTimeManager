4d843c8f49586da1a99f7aa35260438f6fda9c46
v.stepanychev <v.stepanychev@krista.ru>
2026-04-10 11:36:29 +0300

    Spring Rest Controller

---
+++ src/main/java/com/example/UserBaseTest/WorkTimeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.UserBaseTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTimeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkTimeManagerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/UserBaseTest/repository/User.java
@@ -0,0 +1,93 @@
+package com.example.UserBaseTest.repository;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    private String email;
+    private LocalDate birth;
+    private Integer age;
+
+    public User(String name, String email, LocalDate birth, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.birth = birth;
+        this.age = age;
+    }
+
+    public User(String name, String email, LocalDate birth) {
+        this.name = name;
+        this.email = email;
+        this.birth = birth;
+        this.age = Period.between(birth, LocalDate.now()).getYears();
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirth() {
+        return birth;
+    }
+
+    public void setBirth(LocalDate birth) {
+        this.birth = birth;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birth=" + birth +
+                ", age=" + age +
+                '}';
+    }
+}
