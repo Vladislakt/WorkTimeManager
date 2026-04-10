2c0c17666af63e7b0f5df66cd57daa60b3d11698
v.stepanychev <v.stepanychev@krista.ru>
2026-04-10 14:43:10 +0300

    Добавление разрешения на передачу данных

--- backend/src/main/java/com/example/UserBaseTest/controller/UserController.java
+++ backend/src/main/java/com/example/UserBaseTest/controller/UserController.java
@@ -2,20 +2,13 @@ package com.example.UserBaseTest.controller;
 
 import com.example.UserBaseTest.repository.User;
 import com.example.UserBaseTest.service.UserService;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/users")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     private final UserService userService;
